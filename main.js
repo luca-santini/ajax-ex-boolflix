c7212036b69a97e067815fb937ab23f0315366b5
Luca Santini <lucasantinidev@gmail.com>
2020-05-22 17:13:34 +0200

    ciclo for

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 // al click sul pulsante far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
 
-
+// https://api.themoviedb.org/3/search/movie?api_key=6807f8ca279b3a72a7b18a38904c3e42&query=ritorno+al+futuro
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
         'method': 'GET',
@@ -11,12 +11,20 @@ $(document).ready(function() {
             'language': 'it'
         },
         'success': function(data) {
-            console.log(data);
+            var movies = data.results;
+            for (var i = 0; i < movies.length; i++) {
+            var movie = movie[i];
+            console.log(movie);
+
+            var dati_movies = {
+                'titolo': movie.title,
+                'titolo originale': movie.original_title,
+                'lingua': movie.original_language,
+                'voto': movie.vote_average
+            }
         },
         'error': function() {
             alert('si è verificato un errore');
         }
     });
     });
-
-    // https://api.themoviedb.org/3/search/movie?api_key=6807f8ca279b3a72a7b18a38904c3e42&query=ritorno+al+futuro
