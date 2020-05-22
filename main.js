418d8094f7bb8d66db6a4e32e08153e68925c0e2
Luca Santini <lucasantinidev@gmail.com>
2020-05-22 17:04:28 +0200

    console.log(data)

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 // al click sul pulsante far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
 
-var query = 'inception';
 
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
         'method': 'GET',
         'data': {
             'api_key': '6807f8ca279b3a72a7b18a38904c3e42',
-            'query': query,
+            'query': "ritorno al futuro",
             'language': 'it'
         },
         'success': function(data) {
-            var query = data.response;
-            console.log(data.response);
+            console.log(data);
         },
         'error': function() {
             alert('si è verificato un errore');
         }
     });
     });
+
+    // https://api.themoviedb.org/3/search/movie?api_key=6807f8ca279b3a72a7b18a38904c3e42&query=ritorno+al+futuro
