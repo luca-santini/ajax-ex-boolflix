6a11cffbf58a906d51bcbf60890a0060e3a67b58
Luca Santini <lucasantinidev@gmail.com>
2020-05-22 16:24:20 +0200

    chiamata ajax non riuscita

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 // al click sul pulsante far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
-$('#target').click(function() {
-    alert("ok");
-});
+
+var query = 'inception';
+
     $.ajax({
         'url': 'https://api.themoviedb.org/3/search/movie',
         'method': 'GET',
         'data': {
             'api': '6807f8ca279b3a72a7b18a38904c3e42',
-            'query': 'ricerca',
+            'query': query,
             'language': 'it'
         },
-        'success': function(riposta) {
-            console.log(riposta);
-
-        },
-            'error': function() {
-                alert('si è verificato un errore');
+        'success':
+        {
+        $('#target').click(function() {
+            console.log(query);
         }
+        },
+        'error': function() {
+                alert('si è verificato un errore');
+            }
     });
 
 });
