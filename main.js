1f99feaafcf6f46c923b18eb5692d7c15c566356
Luca Santini <lucasantinidev@gmail.com>
2020-05-22 17:48:59 +0200

    var search

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
-// al click sul pulsante far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+    $('button').click(function() {
+        var search = $('#search').val();
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/search/movie',
+            'method': 'GET',
+            'data': {
+                'api_key': '6807f8ca279b3a72a7b18a38904c3e42',
+                'query': search,
+                'language': 'it'
+            },
+            'success': function(data) {
+                var movies = data.results;
 
-// https://api.themoviedb.org/3/search/movie?api_key=6807f8ca279b3a72a7b18a38904c3e42&query=ritorno+al+futuro
-    $.ajax({
-        'url': 'https://api.themoviedb.org/3/search/movie',
-        'method': 'GET',
-        'data': {
-            'api_key': '6807f8ca279b3a72a7b18a38904c3e42',
-            'query': "ritorno al futuro",
-            'language': 'it'
-        },
-        'success': function(data) {
-            var movies = data.results;
-            for (var i = 0; i < movies.length; i++) {
-            var movie = movie[i];
-            console.log(movie);
-
-            var dati_movies = {
-                'titolo': movie.title,
-                'titolo originale': movie.original_title,
-                'lingua': movie.original_language,
-                'voto': movie.vote_average
+                for (var i = 0; i < movies.length; i++) {
+                    var movie = movies[i];
+                    // Ciclare i risultati e per ogni film restituito, stampando in pagina titolo, titolo originale, lingua, voto
+                    var dati_movies = {
+                        'titolo': movie.title,
+                        'titolo originale': movie.original_title,
+                        'lingua': movie.original_language,
+                        'voto': movie.vote_average
+                    }
+                    console.log(dati_movies);
+                } // ciclo for
+            },
+            'error': function() {
+                alert('si è verificato un errore');
             }
-        },
-        'error': function() {
-            alert('si è verificato un errore');
-        }
+        });
     });
-    });
+});
